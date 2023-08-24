53407c0a588ad3bb7e87e736acdf8a39f04bae43
Abhishek <kvsabhiakhi@gmail.com>
2023-08-24 19:02:52 +0530

    adding webserver installation shell

---
+++ web.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+DATE=$(date +%F)
+LOGDIR= /tmp
+SCRIPT_NAME= $0
+LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+
+USERID= $(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR :: Run as sudo user$N"
+    exit 1
+if
